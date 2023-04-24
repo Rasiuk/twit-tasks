74698e6328b8309ea63cfcb8ace8157d7648cda5
Evgeniy <gerardcobain1@gmail.com>
2023-04-24 14:17:07 +0300

    Update card.js

--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -38,7 +38,7 @@ export const Card = ({ user: { id, tweets, followers, avatar } }) => {
         <Avatar src={avatar} width={80} height={80} alt="avatar" />
       </BoxAvatar>
       <InformationBox>
-        <Subtitle>twits : {tweets}</Subtitle>
+        <Subtitle>tweets : {tweets}</Subtitle>
         <Subtitle>{followersNum.toLocaleString("en-US")} followers</Subtitle>
         {!isFollowing ? (
           <Button
